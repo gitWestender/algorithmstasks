85c5b35f83bb4e8280bc3bbd0024407da38ad178
gitWestender <javaxwestender@gmail.com>
2023-12-15 15:06:04 +0300

    complete

--- src/main/java/org/example/model/StringList.java
+++ src/main/java/org/example/model/StringList.java
@@ -1,80 +1,160 @@
 package org.example.model;
 
+import org.example.exceptions.*;
 import org.example.interfaces.IStringList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StringList implements IStringList {
-    List<String> list = new ArrayList<String>();
+    private final String[] list;
+    private int size;
+
+    public StringList() {
+        list = new String[10];
+    }
+
+    public StringList(int size) {
+        list = new String[size];
+    }
 
     @Override
     public String add(String item) {
-        return null;
+        validateSize();
+        validateItem(item);
+        list[size++] = item;
+        return item;
     }
 
     @Override
     public String add(int index, String item) {
-        return null;
+        validateSize();
+        validateIndex(index);
+        validateItem(item);
+
+        if (index == size) {
+            list[size++] = item;
+            return item;
+        }
+
+        System.arraycopy(list, index, list, index + 1, size - index);
+        list[index] = item;
+        size++;
+
+        return item;
     }
 
     @Override
     public String set(int index, String item) {
-        return null;
+        validateIndex(index);
+        validateItem(item);
+        list[index] = item;
+        return item;
     }
 
     @Override
     public String remove(String item) {
-        return null;
+        validateItem(item);
+        int index = indexOf(item);
+
+        if (index == -1) {
+            throw new ElementNotFoundException();
+        }
+
+        if (index != size) {
+            System.arraycopy(list,index+1,list,index, size - (index+1));
+        }
+
+        size--;
+        return item;
     }
 
     @Override
     public String remove(int index) {
-        return null;
+        validateIndex(index);
+
+        String item = list[index];
+
+        if (index != size) {
+            System.arraycopy(list,index+1,list,index, size - (index+1));
+        }
+
+        size--;
+        return item;
     }
 
     @Override
     public boolean contains(String item) {
-        return false;
+        return indexOf(item) != -1;
     }
 
     @Override
     public int indexOf(String item) {
-        return 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(String item) {
-        return 0;
+        for (int i = size - 1; i > 0; i--) {
+            if (list[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public String get(int index) {
-        return null;
+        validateIndex(index);
+        return list[index];
     }
 
     @Override
     public boolean equals(StringList otherList) {
-        return false;
+        return Arrays.equals(this.toArray(), otherList.toArray());
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
-
+        size = 0;
     }
 
     @Override
     public String[] toArray() {
-        return new String[0];
+        return Arrays.copyOf(list, size);
+    }
+
+    private void validateSize() {
+        if (size == list.length) {
+            throw new ListIsFullException();
+        }
+    }
+
+    private void validateItem(String item) {
+        if (item == null) {
+            throw new NullItemException();
+        }
+    }
+
+    private void validateIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new InvalidIndexException();
+        }
     }
 }
