d0702713ecdccdf6304d43a42c319a82cdd0bc07
gitWestender <javaxwestender@gmail.com>
2023-12-15 10:36:54 +0300

    add

---
+++ src/main/java/org/example/model/StringList.java
@@ -0,0 +1,80 @@
+package org.example.model;
+
+import org.example.interfaces.IStringList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringList implements IStringList {
+    List<String> list = new ArrayList<String>();
+
+    @Override
+    public String add(String item) {
+        return null;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return new String[0];
+    }
+}
